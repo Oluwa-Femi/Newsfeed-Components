68debc2464d2b33c5ac912b07b070833aa410d60
Oluwa-femi <oluwatolablessing@gmail.com>
2019-09-04 15:32:27 +0100

    Menuexecution defined

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -8,6 +8,32 @@ let menuItems = [
   'Music',
   'Log Out'
 ];
+ 
+let header = document.querySelector('.header');
+let body = document.querySelector('body');
+let menuButton = document.querySelector('.menu-button');
+
+function menuExecutor(array) {
+  let div = document.createElement('div');
+  let ul = document.createElement('ul');
+  header.appendChild(div);
+  div.classList.add('menu');
+
+  array.forEach((item) => {
+    let li = document.createElement('li');
+    li.textContent = item;
+    ul.appendChild(li);
+  })
+  div.appendChild(ul);
+
+  menuButton.addEventListener('click', (event) => {
+    let menu = document.querySelector('.menu');
+    menu.classList.toggle('menu--open');
+  })
+
+  return div;
+}
+
 
 /* 
 
